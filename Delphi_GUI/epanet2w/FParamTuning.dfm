36a2ed5ab6c4aaa6a1f0ec7a587f93cf6d79c8db
Yuming Liu <jercy_lew@yahoo.com>
2024-06-29 08:32:05 +0800

    Fill data to parameter tuning control lists

--- Delphi_GUI/epanet2w/FParamTuning.dfm
+++ Delphi_GUI/epanet2w/FParamTuning.dfm
@@ -4,7 +4,7 @@ object ParamTuningForm: TParamTuningForm
   Hint = 'Parameters tunning for nodes and links'
   BorderStyle = bsDialog
   Caption = 'Parameters Tuning'
-  ClientHeight = 415
+  ClientHeight = 408
   ClientWidth = 645
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -15,6 +15,335 @@ object ParamTuningForm: TParamTuningForm
   KeyPreview = True
   OldCreateOrder = False
   Position = poMainFormCenter
+  OnClose = FormClose
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 40
+    Top = 59
+    Width = 77
+    Height = 13
+    Caption = 'Parameter Type'
+  end
+  object Label2: TLabel
+    Left = 384
+    Top = 59
+    Width = 32
+    Height = 13
+    Caption = 'Object'
+  end
+  object GroupBox1: TGroupBox
+    Left = 24
+    Top = 24
+    Width = 577
+    Height = 89
+    Caption = 'Choose an object'
+    TabOrder = 2
+  end
+  object cmbParmType: TComboBox
+    Left = 131
+    Top = 56
+    Width = 145
+    Height = 22
+    Style = csOwnerDrawFixed
+    ItemIndex = 6
+    TabOrder = 0
+    Text = 'All'
+    OnChange = cmbParmTypeChange
+    Items.Strings = (
+      'Junction'
+      'Reservoir'
+      'Tank'
+      'Pipe'
+      'Pump'
+      'Valve'
+      'All')
+  end
+  object cmbObject: TComboBox
+    Left = 440
+    Top = 56
+    Width = 145
+    Height = 22
+    Style = csOwnerDrawFixed
+    TabOrder = 1
+    OnChange = cmbObjectChange
+  end
+  object GroupBox2: TGroupBox
+    Left = 24
+    Top = 143
+    Width = 577
+    Height = 257
+    Caption = 'Parameters Tunning'
+    TabOrder = 3
+    object lblParam1: TLabel
+      Left = 16
+      Top = 36
+      Width = 56
+      Height = 13
+      Caption = 'Parameter1'
+    end
+    object Label4: TLabel
+      Left = 340
+      Top = 40
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object lblParam2: TLabel
+      Left = 16
+      Top = 65
+      Width = 56
+      Height = 13
+      Caption = 'Parameter2'
+    end
+    object lblParam3: TLabel
+      Left = 16
+      Top = 93
+      Width = 56
+      Height = 13
+      Caption = 'Parameter3'
+    end
+    object lblParam4: TLabel
+      Left = 16
+      Top = 120
+      Width = 56
+      Height = 13
+      Caption = 'Parameter4'
+    end
+    object lblParam5: TLabel
+      Left = 16
+      Top = 148
+      Width = 56
+      Height = 13
+      Caption = 'Parameter5'
+    end
+    object lblParam6: TLabel
+      Left = 16
+      Top = 175
+      Width = 56
+      Height = 13
+      Caption = 'Parameter6'
+    end
+    object Label10: TLabel
+      Left = 340
+      Top = 65
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object Label11: TLabel
+      Left = 340
+      Top = 93
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object Label12: TLabel
+      Left = 340
+      Top = 120
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object Label13: TLabel
+      Left = 340
+      Top = 152
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object Label14: TLabel
+      Left = 340
+      Top = 171
+      Width = 22
+      Height = 13
+      Caption = 'Step'
+    end
+    object scrlbVal1: TScrollBar
+      Left = 140
+      Top = 36
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 0
+      OnChange = scrlbVal1Change
+    end
+    object edtVal1: TEdit
+      Left = 448
+      Top = 37
+      Width = 85
+      Height = 21
+      TabOrder = 1
+      Text = '0'
+      OnChange = edtVal1Change
+    end
+    object edtStep1: TEdit
+      Left = 368
+      Top = 37
+      Width = 35
+      Height = 21
+      TabOrder = 2
+      Text = '1'
+    end
+    object scrlbVal2: TScrollBar
+      Left = 140
+      Top = 63
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 3
+      OnChange = scrlbVal2Change
+    end
+    object scrlbVal3: TScrollBar
+      Left = 140
+      Top = 90
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 4
+      OnChange = scrlbVal3Change
+    end
+    object scrlbVal4: TScrollBar
+      Left = 140
+      Top = 117
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 5
+      OnChange = scrlbVal4Change
+    end
+    object scrlbVal5: TScrollBar
+      Left = 140
+      Top = 144
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 6
+      OnChange = scrlbVal5Change
+    end
+    object scrlbVal6: TScrollBar
+      Left = 140
+      Top = 171
+      Width = 190
+      Height = 21
+      Max = 1000
+      PageSize = 0
+      TabOrder = 7
+      OnChange = scrlbVal6Change
+    end
+    object edtStep2: TEdit
+      Left = 368
+      Top = 64
+      Width = 35
+      Height = 21
+      TabOrder = 8
+      Text = '1'
+    end
+    object edtStep3: TEdit
+      Left = 368
+      Top = 91
+      Width = 35
+      Height = 21
+      TabOrder = 9
+      Text = '1'
+    end
+    object edtStep4: TEdit
+      Left = 368
+      Top = 118
+      Width = 35
+      Height = 21
+      TabOrder = 10
+      Text = '1'
+    end
+    object edtStep5: TEdit
+      Left = 368
+      Top = 145
+      Width = 35
+      Height = 21
+      TabOrder = 11
+      Text = '1'
+    end
+    object edtStep6: TEdit
+      Left = 368
+      Top = 172
+      Width = 35
+      Height = 21
+      TabOrder = 12
+      Text = '1'
+    end
+    object edtVal2: TEdit
+      Left = 448
+      Top = 64
+      Width = 85
+      Height = 21
+      TabOrder = 13
+      Text = '0'
+      OnChange = edtVal2Change
+    end
+    object edtVal3: TEdit
+      Left = 448
+      Top = 91
+      Width = 85
+      Height = 21
+      TabOrder = 14
+      Text = '0'
+      OnChange = edtVal3Change
+    end
+    object edtVal4: TEdit
+      Left = 448
+      Top = 120
+      Width = 85
+      Height = 21
+      TabOrder = 15
+      Text = '0'
+      OnChange = edtVal4Change
+    end
+    object edtVal5: TEdit
+      Left = 448
+      Top = 147
+      Width = 85
+      Height = 21
+      TabOrder = 16
+      Text = '0'
+      OnChange = edtVal5Change
+    end
+    object edtVal6: TEdit
+      Left = 448
+      Top = 174
+      Width = 85
+      Height = 21
+      TabOrder = 17
+      Text = '0'
+      OnChange = edtVal6Change
+    end
+  end
+  object GridPanel1: TGridPanel
+    Left = 632
+    Top = 408
+    Width = 185
+    Height = 41
+    Caption = 'GridPanel1'
+    ColumnCollection = <
+      item
+        Value = 50.000000000000000000
+      end
+      item
+        Value = 50.000000000000000000
+      end>
+    ControlCollection = <>
+    RowCollection = <
+      item
+        Value = 50.000000000000000000
+      end
+      item
+        Value = 50.000000000000000000
+      end>
+    TabOrder = 4
+  end
 end
--- Delphi_GUI/epanet2w/FParamTuning.pas
+++ Delphi_GUI/epanet2w/FParamTuning.pas
@@ -4,10 +4,68 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Uglobals;
+
+const
+  objTag: array[JUNCS..VALVES]  of PChar = ('Junc ', 'Resvr ', 'Tank ', 'Pipe ', 'Pump ', 'Valve ');
 
 type
   TParamTuningForm = class(TForm)
+    cmbParmType: TComboBox;
+    Label1: TLabel;
+    cmbObject: TComboBox;
+    Label2: TLabel;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    lblParam1: TLabel;
+    scrlbVal1: TScrollBar;
+    edtVal1: TEdit;
+    Label4: TLabel;
+    edtStep1: TEdit;
+    lblParam2: TLabel;
+    lblParam3: TLabel;
+    lblParam4: TLabel;
+    lblParam5: TLabel;
+    lblParam6: TLabel;
+    scrlbVal2: TScrollBar;
+    scrlbVal3: TScrollBar;
+    scrlbVal4: TScrollBar;
+    scrlbVal5: TScrollBar;
+    scrlbVal6: TScrollBar;
+    GridPanel1: TGridPanel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    edtStep2: TEdit;
+    edtStep3: TEdit;
+    edtStep4: TEdit;
+    edtStep5: TEdit;
+    edtStep6: TEdit;
+    edtVal2: TEdit;
+    edtVal3: TEdit;
+    edtVal4: TEdit;
+    edtVal5: TEdit;
+    edtVal6: TEdit;
+    procedure scrlbVal1Change(Sender: TObject);
+    procedure scrlbVal2Change(Sender: TObject);
+    procedure scrlbVal3Change(Sender: TObject);
+    procedure scrlbVal4Change(Sender: TObject);
+    procedure scrlbVal5Change(Sender: TObject);
+    procedure scrlbVal6Change(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure PopulateObjectList(ObjType: Integer);
+    procedure UpdateTuningParamLabelNames(ObjType: Integer);
+    procedure edtVal1Change(Sender: TObject);
+    procedure edtVal2Change(Sender: TObject);
+    procedure edtVal3Change(Sender: TObject);
+    procedure edtVal4Change(Sender: TObject);
+    procedure edtVal5Change(Sender: TObject);
+    procedure edtVal6Change(Sender: TObject);
+    procedure cmbParmTypeChange(Sender: TObject);
+    procedure cmbObjectChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +78,257 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TParamTuningForm.FormCreate(Sender: TObject);
+var i: Integer;
+begin
+  if (cmbParmType.ItemIndex = 6) then
+    begin
+      for i := JUNCS to VALVES do
+        begin
+              PopulateObjectList(i);
+        end;
+
+      UpdateTuningParamLabelNames(JUNCS);
+    end
+  else
+    begin
+        PopulateObjectList(cmbParmType.ItemIndex);
+        UpdateTuningParamLabelNames(cmbParmType.ItemIndex);
+    end;
+
+
+  cmbObject.ItemIndex := 0
+end;
+
+
+procedure TParamTuningForm.PopulateObjectList(ObjType: Integer);
+var i, n: Integer; objIdTxt: String;
+begin
+    n := Network.Lists[ObjType].Count;
+    for i := 0 to n-1 do
+      begin
+            objIdTxt := objTag[ObjType] + GetID(ObjType,i);
+            cmbObject.Items.Add(objIdTxt)
+      end;
+end;
+
+
+procedure TParamTuningForm.UpdateTuningParamLabelNames(ObjType: Integer);
+begin
+       //Junction: Elevation, Base Demand, Emitter Coeff., Initial Quality,
+  //          Source Quality
+  //Reservoir:  Total Head,  Initial Quality, Source Quality
+  //Tank: Elevation, Initial Level, Minimum Level, Maximum Level, Diameter,
+  //      Minimum Volume, Mixing Fraction, Reaction Coeff.,  Initial Quality, Source Quality
+  //Pipe: Length, Diameter, Roughness, Loss Coeff., Bulk Coeff., Wall Coeff.
+  //Pump: Power, Speed,  Energy Price
+  //Valve: Diameter, Setting, Loss Coeff.,
+  case objType of
+    JUNCS:  begin
+          lblParam1.Caption := 'Elevation';
+          lblParam2.Caption := 'Base Demand';
+          lblParam3.Caption := 'Emitter Coeff.';
+          lblParam4.Caption := 'Initial Quality';
+          lblParam5.Caption := 'Source Quality';
+          lblParam6.Caption := 'NA';
+    end;
+
+    RESERVS:  begin
+          lblParam1.Caption := 'Total Head';
+          lblParam2.Caption := 'Initial Quality';
+          lblParam3.Caption := 'Source Quality';
+          lblParam4.Caption := 'NA';
+          lblParam5.Caption := 'NA';
+          lblParam6.Caption := 'NA';
+    end;
+
+    TANKS:  begin
+          lblParam1.Caption := 'Elevation';
+          lblParam2.Caption := 'Initial Level';
+          lblParam3.Caption := 'Minimum Level';
+          lblParam4.Caption := 'Diameter';
+          lblParam5.Caption := 'Minimum Volume';
+          lblParam6.Caption := 'Mixing Fraction';
+    end;
+
+    PIPES: begin
+          lblParam1.Caption := 'Length';
+          lblParam2.Caption := 'Diameter';
+          lblParam3.Caption := 'Roughness';
+          lblParam4.Caption := 'Loss Coeff.';
+          lblParam5.Caption := 'Bulk Coeff.';
+          lblParam6.Caption := 'Wall Coeff.';
+    end;
+
+    PUMPS: begin
+          lblParam1.Caption := 'Power';
+          lblParam2.Caption := 'Speed';
+          lblParam3.Caption := 'Energy Price';
+          lblParam4.Caption := 'NA';
+          lblParam5.Caption := 'NA';
+          lblParam6.Caption := 'NA';
+    end;
+
+    VALVES: begin
+          lblParam1.Caption := 'Diameter';
+          lblParam2.Caption := 'Setting';
+          lblParam3.Caption := 'Emitter Coeff.';
+          lblParam4.Caption := 'Loss Coeff.';
+          lblParam5.Caption := 'NA';
+          lblParam6.Caption := 'NA';
+    end;
+  end;
+end;
+
+
+procedure TParamTuningForm.cmbObjectChange(Sender: TObject);
+var i: Integer;
+begin
+      //Load the properties for the select object
+
+      //Fill these properties to this parameter tuning view
+
+
+end;
+
+procedure TParamTuningForm.cmbParmTypeChange(Sender: TObject);
+var i, objType: Integer;
+begin
+  objType := cmbParmType.ItemIndex;
+  cmbObject.Items.Clear;
+  if (objType = 6) then
+      begin
+        for i := JUNCS to VALVES do
+          begin
+                PopulateObjectList(i);
+          end;
+          UpdateTuningParamLabelNames(JUNCS);
+      end
+    else
+    begin
+         PopulateObjectList(objType);
+         UpdateTuningParamLabelNames(objType);
+    end;
+
+    cmbObject.ItemIndex := 0;
+end;
+
+procedure TParamTuningForm.edtVal1Change(Sender: TObject);
+var inputVal, bOk: Integer; stepTxt, valTxt: String; step: Real;
+begin
+      valTxt :=  edtVal1.Text;
+      stepTxt := edtStep1.Text;
+
+      if (valTxt = '') or (stepTxt = '') then
+        Exit;
+
+      Val(valTxt, inputVal, bOk);
+      if (bOk <> 0) then
+        Exit;
+
+      Val(stepTxt, step, bOk);
+      if (bOk <> 0) then
+        Exit;
+
+      if (inputVal >= 0) and (inputVal <= 1000) then
+        scrlbVal1.Position :=  ROUND(inputVal/step);
+end;
+
+procedure TParamTuningForm.edtVal2Change(Sender: TObject);
+var inputVal: Integer; s: String;
+begin
+
+end;
+
+procedure TParamTuningForm.edtVal3Change(Sender: TObject);
+var inputVal: Integer; s: String;
+begin
+
+end;
+
+procedure TParamTuningForm.edtVal4Change(Sender: TObject);
+var inputVal: Integer; s: String;
+begin
+
+end;
+
+procedure TParamTuningForm.edtVal5Change(Sender: TObject);
+var inputVal: Integer; s: String;
+begin
+
+end;
+
+procedure TParamTuningForm.edtVal6Change(Sender: TObject);
+var inputVal: Integer; s: String;
+begin
+
+end;
+
+procedure TParamTuningForm.FormClose(Sender: TObject; var Action: TCloseAction);
+var i: Integer;
+begin
+
+end;
+
+
+procedure TParamTuningForm.scrlbVal1Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+        inputVal :=  scrlbVal1.Position;
+        step := StrToFloat(edtStep1.Text);
+        edtVal1.Text := FloatToStr(inputVal*step);
+
+        //Save the updated property
+
+        //Re-run the simulation
+  end;
+
+
+procedure TParamTuningForm.scrlbVal2Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+      inputVal :=  scrlbVal2.Position;
+      step := StrToFloat(edtStep2.Text);
+      edtVal2.Text := FloatToStr(inputVal*step)
+  end;
+
+procedure TParamTuningForm.scrlbVal3Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+      inputVal :=  scrlbVal3.Position;
+      step := StrToFloat(edtStep3.Text);
+      edtVal3.Text := FloatToStr(inputVal*step)
+  end;
+
+procedure TParamTuningForm.scrlbVal4Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+      inputVal :=  scrlbVal4.Position;
+      step := StrToFloat(edtStep4.Text);
+      edtVal4.Text := FloatToStr(inputVal*step)
+  end;
+
+procedure TParamTuningForm.scrlbVal5Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+      inputVal :=  scrlbVal5.Position;
+      step := StrToFloat(edtStep5.Text);
+      edtVal5.Text := FloatToStr(inputVal*step)
+  end;
+
+procedure TParamTuningForm.scrlbVal6Change(Sender: TObject);
+  var inputVal: Integer;
+  var step: Real;
+  begin
+      inputVal :=  scrlbVal6.Position;
+      step := StrToFloat(edtStep6.Text);
+      edtVal6.Text := FloatToStr(inputVal*step)
+  end;
 
 end.
